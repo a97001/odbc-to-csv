a9364246962b1a27b83fd0b0a2339cee9965e443
a97001 <a97001@gmail.com>
2019-01-02 01:23:49 +0800

    dev

---
+++ app.py
@@ -0,0 +1,21 @@
+import datetime
+import json
+import pyodbc
+import pandas as pd
+
+with open('./config.json', 'r') as f:
+    config = json.load(f)
+
+print(pyodbc.drivers())
+
+cnxn = pyodbc.connect(config['connString'])
+df = pd.read_sql("SELECT * FROM [test].[dbo].[data]", cnxn)
+df = df.set_index('timestamp')
+# test = df.T
+# print(test.index)
+df.to_csv('data_' + str(datetime.datetime.now().timestamp()) + '.csv')
+
+# cursor = cnxn.cursor()
+# cursor.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'")
+# rows = cursor.fetchall()
+# print(rows)
